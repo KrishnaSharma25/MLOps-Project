680408805b22e4bc2bcc1e714197a7192f6da467
KrishnaSharma25 <64472620+KrishnaSharma25@users.noreply.github.com>
2020-06-28 11:42:57 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -105,7 +105,7 @@ print("Sending mail...")
 import smtplib
 s=smtplib.SMTP('smtp.gmail.com',587)
 s.starttls()
-s.login("krishana250799@gmail.com","wiajltodlxtlxfzz")
+s.login("krishana250799@gmail.com","axnbqbhkktszxqza")
 message1="success"
 s.sendmail("krishana250799@gmail.com","krishana250799@gmail.com",message1)
 s.quit()
